1a692d437b2f832c6925687afbb39008d9f31b1d
simonKgithub <yonsei1994@gmail.com>
2023-08-24 17:49:31 +0900

    ~place 컨트롤러 생성

---
+++ src/main/java/com/delivery/controller/PlaceController.java
@@ -0,0 +1,23 @@
+package com.delivery.controller;
+
+import com.delivery.dto.PlaceFormDto;
+import com.delivery.service.PlaceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/place")
+@RequiredArgsConstructor
+public class PlaceController {
+
+    private final PlaceService placeService;
+
+    @GetMapping("/new")
+    public String placeForm(Model model) {
+        model.addAttribute("placeFormDto", new PlaceFormDto());
+        return "place/placeForm";
+    }
+}
